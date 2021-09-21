0675d7f2ac87ec81bbb63ac9f0b6369a0d6f5dc0
Joel <wilddroppings@gmail.com>
2021-09-21 14:17:50 -0600

    Set today's details to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,19 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import arth  from '../../assets/Arth.gif';
 import { getTodaysImageDetails } from '../../apiCalls';
 
 const App = () => {
-  
+
+  const [todaysDetails, setTodaysDetails] = useState({})
+
   useEffect(() => {
     getTodaysImageDetails()
     .then(details => {
+      setTodaysDetails(details)
       console.log(details)
     })
-  })
-
-
-
+  }, [])
 
   return (
     <img src={arth} alt=''/>
