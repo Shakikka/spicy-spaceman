38fb37968dc794b03aa08ff01d067da06863de10
Joel <wilddroppings@gmail.com>
2021-09-21 19:33:27 -0600

    Fix styling for image card

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -8,6 +8,11 @@ const ImageCard = ({ todaysDetails }) => {
         <section className='image-card-box'>
             <img className='todays-image' src={url} alt=''/>
             <h3>{title}</h3>
+            <div className='img-deets'>
+                <h6>{copyright}</h6>
+                <h6>{date}</h6>
+            </div>
+            <h6>{explanation}</h6>
         </section>
     )
 }
