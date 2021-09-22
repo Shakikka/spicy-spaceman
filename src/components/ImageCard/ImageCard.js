1d448ff5df2d5e4b088344af1038acbe1dd845aa
Joel <wilddroppings@gmail.com>
2021-09-21 20:01:36 -0600

    Add like button

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -7,12 +7,13 @@ const ImageCard = ({ todaysDetails }) => {
     return (
         <section className='image-card-box'>
             <img className='todays-image' src={url} alt=''/>
-            <div className='img-deets'>
+            <div className='image-details'>
                 <h6>{copyright}</h6>
                 <h6>{date}</h6>
             </div>
             <h3>{title}</h3>
             <h6>{explanation}</h6>
+            <button>Like?</button>
         </section>
     )
 }
